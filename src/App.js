1b71634ed09cf3f5de18fca87e282f102800b95a
travizjass <ahirjasmin5@gmail.com>
2022-06-06 15:33:49 +0530

    modified and changed styles II

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Card from './components/Cards/Card';
+import About from './components/About/About';
 
 function App() {
   return (
     <>
     <Navbar/>
+    <About/>
     <Card/>
     </>
   );
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg">
     <div className="d-flex container-fluid">
-      <a className="navbar-brand" href="#">Raunak554 ❤️</a>
+      <a className="navbar-brand" href="#">Raunak554 🤖</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
