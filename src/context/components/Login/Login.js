04bf3848b0d38fc250e8920483ae48e9a2610f99
raunak <raunakkumar.india@gmail.com>
2022-07-13 11:58:28 +0530

    structure

--- src/context/components/Login/Login.js
+++ src/context/components/Login/Login.js
@@ -21,7 +21,7 @@ function Login(props) {
   
   const  handleSubmit=async(e)=>{
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/login",{
+    const response = await fetch("https://floating-ocean-72177.herokuapp.com/api/auth/login",{
             method:'POST',
             headers:{
                 'Content-Type':'application/json'
