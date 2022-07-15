4ef71d1e0e9a24e780b2a22451a93a3d62cda75c
raunakkumarsingh <35128126+raunakkumarsingh@users.noreply.github.com>
2022-07-15 20:05:56 +0530

    Update db.js

--- Server/db.js
+++ Server/db.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 
 
 // const mongo_URI=process.env.mongo_URI_Atlas
-const mongo_URI="mongodb+srv://root:toor@cluster0.vh2d3os.mongodb.net/DsaServer"
+const mongo_URI="mongodb+srv://id:password@cluster0.vh2d3os.mongodb.net/DsaServer"
 
    console.log(process.env.mongo_URI_Atlas)
 const connectToMongo=()=>{
